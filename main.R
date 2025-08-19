4d571a5ae6e7ca083dd5dda373301f4ec338dc2a
shug0131 <sjb277@medschl.cam.ac.uk>
2025-08-19 12:46:21 +0100

    Update main.R

--- main.R
+++ main.R
@@ -2,7 +2,7 @@
 # sudo apt-get install libudunits2-dev
 # sudo apt-get install -y  libudunits2-dev libgdal-dev libgeos-dev libproj-dev
 # sudo apt-get install -y libjq-dev
-# sudo apt install default-jdk gdal-bin
+# sudo apt install -y default-jdk gdal-bin
 #renv::install("rStrava")
 # renv::install("leaflet)
 #renv::install("googlePolylines")
@@ -21,8 +21,13 @@ library(jsonlite)
 library(xlsx)
 library(magrittr)
 
-stoken <- httr::config(token = strava_oauth(app_name, app_client_id, app_secret, app_scope="activity:read_all"))
+#https://jessicasalbert.medium.com/holding-your-hand-through-stravas-api-e642d15695f2
 
+tokenurl <- strava_oauth(app_name, app_client_id, app_secret, app_scope="activity:read_all")
+tokenurl <- "ba78c40a92a993e96dbaf976a775ad3e91bae763"
+stoken <- httr::config(token = tokenurl)
+
+get_athlete(stoken, id="3766259")
 
 rides_df <- read.csv(file="activities.csv") %>% 
   na.omit()
@@ -30,7 +35,7 @@ ids <- gsub(".*/(\\d+$)","\\1", rides_df$id)
 ids <- ids[ids!=""]
 
 #rides_df[52:57,]
-extra <-  get_activity_list(stoken, id = ids[52:57])
+extra <-  get_activity_list(stoken, id = ids[1:3])
 load("strava_data.Rds")
 
 my_acts <-c(my_acts, extra) 
